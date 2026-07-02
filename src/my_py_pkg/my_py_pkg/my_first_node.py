402d069c183c60b40c95d347aaaaaf6751e3de7e
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2026-07-02 15:06:23 +0530

    Refactor my_first_node: add MyFirstNode class with timer callback
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -2,10 +2,20 @@
 import rclpy
 from rclpy.node import Node
 
+class MyFirstNode(Node):
+    def __init__(self):
+        super().__init__("my_first_py_node")
+        self.counter_ = 0
+        self.get_logger().info("Hello ROS2")
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello ROS2 " + str(self.counter_))
+        self.counter_ += 1
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("my_first_py_node")
-    node.get_logger().info("Hello ROS2")
+    node = MyFirstNode()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
