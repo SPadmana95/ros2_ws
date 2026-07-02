186f5ff57d20713cbafc006a271cb43f1cb5ba6d
Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>
2026-07-02 16:40:05 +0530

    Fix my_first_node: use std::make_shared to enable timer callback
    Signed-off-by: Sivasubramaniyan Padmanaban <sivasubramaniyan.padmanaban@analog.com>

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,10 +1,29 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyFirstNode : public rclcpp::Node
+{
+public:
+  MyFirstNode() : Node("my_first_node")
+  {
+    RCLCPP_INFO(this->get_logger(), "Hello, ROS 2!");
+    timer_ = this->create_wall_timer(
+      std::chrono::seconds(1),
+      std::bind(&MyFirstNode::timer_callback, this));
+  }
+private:
+  void timer_callback()
+  {
+    RCLCPP_INFO(this->get_logger(), "Timer callback triggered %d times", ++counter_);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  int counter_ = 0;
+
+
+};
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("my_first_node");  
-  RCLCPP_INFO(node->get_logger(), "Hello, ROS 2!");
+  auto node = std::make_shared<MyFirstNode>();
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
